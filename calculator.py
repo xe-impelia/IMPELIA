721b5d669055680c067f1017d9ae3a8cf18548a8
xe-impelia <xavier.estruga.sp@impelia.org>
2023-05-09 16:54:33 +0200

    Canvis en comentary i menu

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+# Calculator sample
+
 # This function adds two numbers
 def add(x, y):
     return x + y
@@ -15,11 +17,11 @@ def divide(x, y):
     return x / y
 
 
-print("Select operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
+print("Select operation:")
+print(" 1.Add")
+print(" 2.Subtract")
+print(" 3.Multiply")
+print(" 4.Divide")
 
 while True:
     # take input from the user
